08b7c21cfa84296152a67576cf0fcc0b25b33f74
Max <maxtarasevich@gmail.com>
2022-01-02 22:20:25 +0300

    init: start order-summary-comp. task with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      'Pale-blue': 'hsl(225, 100%, 94%)',
+      'Bright-blue': 'hsl(245, 75%, 52%)',
+      'Very-pale-blue': 'hsl(225, 100%, 98%)',
+      'Desaturated-blue': 'hsl(224, 23%, 55%)',
+      'Dark-blue': 'hsl(223, 47%, 23%)',
+    },
+    fontFamily: {
+      sans: ['"Red Hat Display"', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
